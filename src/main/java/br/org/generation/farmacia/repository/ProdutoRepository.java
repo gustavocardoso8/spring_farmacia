3dfc3d7bfc75e94a67487635ae0f29e774bdc004
Gustavo Cardoso <contato.gusttavo08@gmail.com>
2022-02-18 00:15:18 -0300

    CRUD finalizado

--- src/main/java/br/org/generation/farmacia/repository/ProdutoRepository.java
+++ src/main/java/br/org/generation/farmacia/repository/ProdutoRepository.java
@@ -11,7 +11,7 @@ import br.org.generation.farmacia.model.Produto;
 @Repository
 public interface ProdutoRepository extends JpaRepository <Produto, Long> {
 	
-	public List<Produto> findAllByNameContainingIgnoreCase (String nome);
+	public List<Produto> findAllByNomeContainingIgnoreCase (String nome);
 	
 	public List<Produto> findByPrecoBetween (BigDecimal start, BigDecimal end);
 
